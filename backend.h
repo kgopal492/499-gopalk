b5de8b4bb15507eaef668a0aec5bfa9a56ac68f5
Krishna Gopal <gopalk@usc.edu>
2019-01-23 21:47:41 -0800

    fixed pull request issues - header indentation & method comments

--- backend.h
+++ backend.h
@@ -4,18 +4,18 @@
 #include <string>
 
 class Backend {
-  public:
-    // put key and value pair in key-value store
-    bool put(const string &key, const string &value);
+ public:
+  // put key and value pair in key-value store
+  bool put(const string &key, const string &value);
 
-    // get value from key string
-    string get(const string &key);
+  // get value based upon key string
+  string get(const string &key);
 
-    // delete key-value pair given key
-    bool delete(const string &key);
+  // delete key-value pair given key
+  bool delete(const string &key);
 
-  private:
-    // store of key value pairs in backend
-    std::map<string, string> key_value_pairs_;
+ private:
+  // store of key value pairs in backend
+  std::map<string, string> key_value_pairs_;
 };
 #endif //CHIRP_BACKEND_H
--- service.h
+++ service.h
@@ -4,29 +4,29 @@
 #include <string>
 
 class Service {
-  public:
-    // register user with backend data store
-    bool registerUser(const string &username);
+ public:
+  // register user with backend data store
+  bool registerUser(const string &username);
 
-    // log user in to command line
-    bool loginUser(const string &username);
+  // log user in to command line
+  bool loginUser(const string &username);
 
-    // broadcast chirp from user to data store
-    bool chirp(const string &text);
+  // broadcast chirp from user to data store
+  bool chirp(const string &text);
 
-    // reply to chirpID (with given data store)
-    bool reply(const string &chirpID);
+  // reply to chirpID (with given data store)
+  bool reply(const string &chirpID);
 
-    // add a user to following list
-    bool follow(const string &username);
+  // add a user to following list
+  bool follow(const string &username);
 
-    // read thread given starting chirpID
-    string read(const string &chirpID);
+  // read thread given starting chirpID
+  string read(const string &chirpID);
 
-    // view stream of chirps from followers
-    bool monitor();
+  // view stream of chirps from followers
+  bool monitor();
 
-  private:
-    // TODO: include followers
+ private:
+  // TODO: include followers
 };
 #endif //CHIRP_SERVICE_H
